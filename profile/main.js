f00840614a7e03ce6100c8295dc8c72e16bf3e22
masato-sado <masato.sado01@gmail.com>
2020-06-07 22:34:56 +0900

    不要なもの削除

--- profile/main.js
+++ profile/main.js
@@ -1,30 +1,3 @@
-// 下にスクロールするとフェードインする処理
-
-// animatedクラスの付いた要素にwaypointを登録
-$('.animated').waypoint({
-  handler(direction) {
-    // 要素が画面中央に来るとここが実行される
-    if (direction === 'down') {
-      /**
-       * 下方向のスクロール
-       * イベント発生元の要素にfadeInUpクラスを付けることで
-       * アニメーションを開始
-       */
-      $(this.element).addClass('fadeInUp');
-
-      /**
-       * waypointを削除することで、この要素に対しては
-       * これ以降handlerが呼ばれなくなる
-       */
-      this.destroy();
-    }
-  },
-
-  // 要素が画面中央に来たらhandlerを実行
-  offset: '80%',
-});
-
-
 // トップに戻る処理
 
 // ボタンの表示、非表示を切り替える関数
